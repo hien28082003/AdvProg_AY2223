36357e8c13472db233c6833d63795a374cddd9d3
hiền <dangthanhhien28082003@gmail.com>
2023-07-08 17:52:57 +0700

    Update hello.cpp
    fix

--- AdvProg_L0-Hello/hello.cpp
+++ AdvProg_L0-Hello/hello.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "hello.h"
-using namespace std;
+
 std::string printGameOver(){
 	// TODO: Return the required string
-	cout<<"Game over !" << endl;
-	return 0;
+	return "Game over !";
 }
